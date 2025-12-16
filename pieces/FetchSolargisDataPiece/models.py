82cb5897838925e40ef8a9405c1bf3b2cbfb9bcc
filipchrvala <108301045+filipchrvala@users.noreply.github.com>
2025-12-16 16:09:25 +0100

    Create models.py

---
+++ pieces/FetchSolargisDataPiece/models.py
@@ -0,0 +1,30 @@
+﻿from pydantic import BaseModel, Field
+
+class InputModel(BaseModel):
+    """
+    Fetch Solargis Data Piece Input Model
+    """
+    input_path: str = Field(
+        title="Path to input data files",
+        default='/home/shared_storage/input_data/InputSolargisFile.csv',
+        description="The path to input meteo data files (supports wildcards: *.docx, *.csv, or directory paths)"
+    )
+
+    input_filetype: str = Field(
+        title="Input file type",
+        default='csv',
+        description="The type of input file: 'docx' for Word documents or 'csv' for CSV files"
+    )    
+    
+class OutputModel(BaseModel):
+    """
+    Fetch Solargis Data Piece Output Model
+    """
+    message: str = Field(
+        default="",
+        description="Output message to log"
+    )
+
+    file_path: str = Field(
+        description="The path to the output CSV file"
+    )
