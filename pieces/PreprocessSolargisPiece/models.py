0fc7751cc13fd7867e1ef39b64e5d035c1b68d9f
filipchrvala <108301045+filipchrvala@users.noreply.github.com>
2025-12-19 19:00:38 +0100

    Add files via upload

---
+++ pieces/PreprocessSolargisPiece/models.py
@@ -0,0 +1,24 @@
+﻿from pydantic import BaseModel, Field
+
+class InputModel(BaseModel):
+    """
+    Preprocess Solargis Data Piece Input Model
+    """
+    input_path: str = Field(
+        title="Path to raw data file",
+        default='/home/shared_storage/data/raw_solargis.csv',
+        description="The path to the input Solargis raw data file "
+    )    
+
+class OutputModel(BaseModel):
+    message: str = Field(
+        default="",
+        description="Output message to log"
+    )
+      
+    file_path: str = Field(
+        description="The path to the output CSV file"
+    )
+    
+    processed_rows: int
+    
